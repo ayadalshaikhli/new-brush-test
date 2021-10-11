c49e81937e53d7c1962f9f05d3e4edc2a7061171
ayadalshaikhli <79942630+ayadalshaikhli@users.noreply.github.com>
2021-10-11 13:51:15 -0700

    first commit

---
+++ components/common/index.ts
@@ -0,0 +1,7 @@
+
+
+export { default as Layout } from "./Layout"
+export { default as Footer } from "./Footer"
+export { default as HomePage } from "./HomePage"
+export { default as Navbar } from "./Navbar"
+export { default as Usernav } from "./Usernav"
